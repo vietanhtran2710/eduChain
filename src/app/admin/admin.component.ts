8d4e80d623136746d2df1f7a2337bb456784d578
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-11-07 20:09:33 +0700

    display blockchain information

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
+import { BlockchainService } from '../services/blockchain.service';
 
 @Component({
   selector: 'app-admin',
@@ -11,23 +12,50 @@ export class AdminComponent implements OnInit {
 
   unverifiedUser: Array<any> = [];
   currentAccount: string = "";
+  contestAddress: string = "";
+  rewardAddress: string = "";
   numberOfUsers: number = 0;
   balances: number = 0;
   loaded = true;
 
   constructor(private userService: UserService,
-              private authService: AuthService) { 
-    this.userService.getUnverifiedUser().subscribe({
-      next: (result: any) => {
-        this.unverifiedUser = result;
-      },
-      error: (err) => {
+              private authService: AuthService,
+              private blockchainService: BlockchainService
+  ) { 
+    if (Object.keys(this.authService.currentUserValue).length !== 0) {
+      this.authService.verifyToken().subscribe({
+        next: (data: any) => {
+          this.currentAccount = data.address;
+          this.userService.getUnverifiedUser().subscribe({
+            next: (result: any) => {
+              this.unverifiedUser = result;
+            },
+            error: (err) => {
+      
+            },
+            complete: () => {
+              console.log('complete')
+            }
+          })
+          this.blockchainService.getEthBalance(this.currentAccount)
+          .then((result: any) => {
+            this.balances = Math.round(result / Math.pow(10, 18) * 100) / 100;
+          })
+          .catch((err) => {
 
-      },
-      complete: () => {
-        console.log('complete')
-      }
-    })
+          })
+          this.contestAddress = this.blockchainService.getContestFactoryAddress();
+          this.rewardAddress = this.blockchainService.getRewardAddress();
+        },
+        error: (err) => {
+
+        },
+        complete: () => {
+
+        }
+      })
+    }
+    
   }
 
   ngOnInit(): void {
---
+++ src/app/services/blockchain.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+const Web3 = require('web3')
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlockchainService {
+  private web3: any;
+  private initialized = false;
+
+  private contestFactoryArtifacts = require('../../../build/contracts/ContestFactory.json');
+  private contestFactory = this.contestFactoryArtifacts.abi;
+  private contestFactoryAddress = this.contestFactoryArtifacts.networks["5777"].address
+
+  private rewardArtifacts = require('../../../build/contracts/LearningReward.json');
+  private reward = this.rewardArtifacts.abi;
+  private rewardAddress = this.rewardArtifacts.networks["5777"].address
+
+  constructor() {
+
+  }
+
+  async initWeb3() {
+    this.web3 = new Web3(Web3.givenProvider || '"ws://localhost:7545"');
+    this.initialized = true;
+  }
+
+  async getEthBalance(address: string) {
+    await this.initWeb3();
+    const that = this;
+    return new Promise((resolve, reject) => {
+      that.web3.eth.getBalance(address)
+      .then((result: any) => {
+        return resolve(result);
+      })
+    })
+  }
+
+  getContestFactoryAddress(): string {
+    return this.contestFactoryAddress
+  }
+
+  getRewardAddress(): string {
+    return this.rewardAddress
+  }
+}
