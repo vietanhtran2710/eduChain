1f626586307b938dce5af501ab7fca834263d264
vietanhtran2710 <vietanhtran.uet@gmail.com>
2022-11-20 16:29:29 +0700

    remove unnecessary Ownable inheritance

--- contracts/ContestFactory.sol
+++ contracts/ContestFactory.sol
@@ -1,28 +1,24 @@
 // SPDX-License-Identifier: MIT
-
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "./LearningReward.sol";
 import "./Contest.sol";
 
 pragma solidity ^0.8.0;
 
-contract ContestFactory is Ownable {
+contract ContestFactory {
     event CreatedContest(address contestAddress);
 
     mapping(address => address[]) ownedContests;
     LearningReward rewardContract;
-    address adminAddress;
     address rewardAddress;
     uint256 totalContest = 0;
 
     constructor(address rewardContractAddress) {
-        adminAddress = msg.sender;
         rewardContract = LearningReward(rewardContractAddress);
         rewardAddress = rewardContractAddress;
     }
 
     function createNewContest(bytes[] memory answers) public returns (address) {
-        Contest contest = new Contest(answers, rewardAddress, adminAddress);
+        Contest contest = new Contest(answers, rewardAddress);
         address contestAddress = address(contest);
         rewardContract.grantRole(rewardContract.CONTRACT_ROLE(), contestAddress);
         contest.transferOwnership(msg.sender);
